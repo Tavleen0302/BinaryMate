39a9db503cc60f930c67e04a6e7622dacb4f50c4
Tavleen Aneja <70829396+Tavleen0302@users.noreply.github.com>
2023-01-21 16:38:03 -0500

    Delete main.java

--- main.java
+++
@@ -1,9 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        // IEEE754 test = new IEEE754("");
-        IEEE754 test2 = new IEEE754(-9.21f);
-    
-        System.out.println("Binary value " + test2.getIEEE754Value());
-        // System.out.println("Decimal value " + test.getDecimalValue());
-    }
-}
