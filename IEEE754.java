4c4ce7a84ed3137baecba3f74be79821d2279d0c
Tavleen Aneja <70829396+Tavleen0302@users.noreply.github.com>
2023-01-21 16:31:10 -0500

    Add files via upload

--- IEEE754.java
+++ IEEE754.java
@@ -1,10 +1,8 @@
-import java.lang.StringBuilder;
-
 public class IEEE754 {
+
     private String bits;
     private float number;
 
-    // constructors
     public IEEE754(String bits) {
         this.bits = bits;
     }
@@ -13,87 +11,92 @@ public class IEEE754 {
         this.number = number;
     }
 
-    // methods
     public float getDecimalValue() {
-        float result;
-        // signbit
-        if (bits.charAt(0) == '1') {
-            result = -1;
-        } else {
-            result = 1;
+        float signDecimal = (float) Math.pow(-1, Float.parseFloat(bits.substring(0, 1)));
+        String biasedBinary = bits.substring(1, 9);
+        String mantissaBinary = bits.substring(9, 32);
+        float biasedDecimal = Float.parseFloat(biasedBinary);
+        double mantissaDecimal = Double.parseDouble(mantissaBinary);
+
+        float answerBiasedDecimal = 0;
+        int counter = 0;
+        long remainder = 0;
+        while (biasedDecimal != 0) {
+            remainder = (long) (biasedDecimal % 10);
+            biasedDecimal /= 10;
+            answerBiasedDecimal += remainder * Math.pow(2, counter);
+            ++counter;
         }
-        result *= (1 + convertDecimal(Double.parseDouble(bits.substring(9))) / ( Math.pow(2, 23)));
-        result *= (Math.pow(2, convertDecimal(Float.parseFloat(bits.substring(1, 9))) - 127));
-        return result;
+        int answer = (int) (answerBiasedDecimal - 127);
+
+        float answerMantissaDecimal = 0;
+        int counter2 = 0;
+        long remainder2 = 0;
+        while (mantissaDecimal != 0) {
+            remainder2 = (long) (mantissaDecimal % 10);
+            mantissaDecimal /= 10;
+            answerMantissaDecimal += remainder2 * Math.pow(2, counter2);
+            counter2++;
+            if (counter2 == 23) {
+                break;
+            }
+        }
+        float DecimalValue = (float) ((signDecimal) * (Math.pow(2, answer))
+                * ((answerMantissaDecimal / Math.pow(2, 23)) + 1));
+        System.out.println("The Decimal Value of this binary number is " + (float) (DecimalValue));
+        return 0;
     }
 
     public String getIEEE754Value() {
-        int exponent = 0;
-        String result = "";
-        float normalized;
+        int exponentCounter = 0;
+        int counter = 0;
+        String sign = "";
+        String mantissaList = "";
+        String biasedExponentBinary = "";
 
-        // bit sign
         if (number >= 0) {
-            result += "0";
-        } else {
-            result += "1";
+            sign = "0";
+        } else if (number < 0) {
+            sign = "1";
         }
 
-        // exponent
-        number = Math.abs(number);
-        if (number > 1) {
-            while (number >= 2) {
-                number /= 2;
-                exponent++;
+        float absolute = (float) Math.abs(number);
+        if (absolute < 1) {
+            while (absolute <= 1) {
+                absolute *= 2;
+                exponentCounter -= 1;
             }
-            result += convertExponent(exponent + 127);
+        } else if (absolute > 1) {
+            while (absolute >= 2) {
+                exponentCounter++;
+                absolute /= 2;
+            }
+        }
+        if (exponentCounter <= 0) {
+            biasedExponentBinary = "0" + Integer.toBinaryString(127 + exponentCounter);
         } else {
-            while (number < 1) {
-                number *= 2;
-                exponent++;
-            }
-            result += convertExponent(127 - exponent);
+            biasedExponentBinary += Integer.toBinaryString(127 + exponentCounter);
         }
 
-        // mantissa
-        normalized = number - 1;
-        for (int i = 0; i < 23; i++) {
-            normalized *= 2;
-            if (normalized >= 1) {
-                result += "1";
-                normalized--;
-            } else {
-                result += "0";
+        absolute %= 1;
+        while (counter < 23) {
+            counter++;
+            absolute *= 2;
+            if (absolute >= 1) {
+                mantissaList += "1";
+                absolute %= 1;
+            } else if (absolute < 1) {
+                mantissaList += "0";
             }
         }
-        return result;
+        System.out.println("The Binary value of this number is " + sign + biasedExponentBinary + mantissaList);
+        return " ";
     }
 
-    public float convertDecimal(double binary) {
-        float result = 0;
-        int exponent = 0;
-        while (binary > 0) {
-            result += binary % 10 * Math.pow(2, exponent);
-            binary = Math.floor(binary / 10);
-            exponent++;
-        }
-        return result;
-    }
-
-    public String convertExponent(int biasExponent) {
-        String result = "";
-        int exponent = biasExponent;
-        for (int i = 0; i < 8; i++) {
-            result += (exponent % 2);
-            exponent /= 2;
-        }
-
-        // revserse string
-        StringBuilder reverse = new StringBuilder();
-        reverse.append(result);
-        reverse.reverse();
-        result = reverse.toString();
-        return result;
-
+    public static void main(String[] args) {
+        IEEE754 test = new IEEE754(2f);
+        test.getIEEE754Value();
+        IEEE754 test2 = new IEEE754("10111111100000000000000000000000");
+        test2.getDecimalValue();
     }
 }
